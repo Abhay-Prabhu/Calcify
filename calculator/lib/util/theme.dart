dcf00b5b1f748143fd70a6b9aa2417ce8e1e6d3c
Abhay B Prabhu <abhayprabhu1234@gmail.com>
2024-03-20 12:06:22 +0530

    calculator

---
+++ calculator/lib/util/theme.dart
@@ -0,0 +1,11 @@
+import "package:flutter/material.dart";
+
+const background = Color.fromRGBO(60, 60, 60, 1);
+
+const container1 =Color.fromRGBO(75, 75, 75,1);
+
+const color1 = Color.fromRGBO(131, 125, 125, 1);
+
+const color3 = Color.fromRGBO(75, 75, 75,1);
+
+const color2 = Color.fromRGBO(243, 89, 0, 1);
